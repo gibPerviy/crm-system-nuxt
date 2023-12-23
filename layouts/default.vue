6ead35c78d61a05d64d04d8cfbf0e0bf396298fc
Butenko Bogdan <butenko.bogdan@mail.ru>
2023-12-23 22:39:18 +0300

    clear proj nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+console.log('Loaded')
+</script>
+
+<template>
+  <section class="grid" style="min-height: 100dvh">
+    <aside>sidebar</aside>
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+</style>
